939b7d733b16d968af9b521b3efafbb918127357
Pascal Allau <pascal.allau@zohomail.eu>
2022-08-29 16:31:23 +0200

    add properties to String prototype

--- src/utils/toNormalize.js
+++ src/utils/toNormalize.js
@@ -1,5 +1,34 @@
-String.prototype.toNormalize = function () {
-    return this.normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .toLowerCase();
-};
+// String.prototype.toNormalize = function () {
+//     return this.normalize('NFD')
+//         .replace(/[\u0300-\u036f]/g, '')
+//         .toLowerCase();
+// };
+
+/* Adding methods to the String prototype. */
+Object.defineProperties(String.prototype, {
+    toCapitalize: {
+        value() {
+            const lowerCase = this.toLowerCase();
+            return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+        },
+    },
+    toNormalize: {
+        value() {
+            return this.normalize('NFD')
+                .replace(/[\u0300-\u036f]/g, '')
+                .toLowerCase();
+        },
+    },
+    toTitleCase: {
+        value() {
+            return this.toLowerCase()
+                .split(' ')
+                .map((word) =>
+                    word.length > 2
+                        ? word.charAt(0).toUpperCase() + word.slice(1)
+                        : word
+                )
+                .join(' ');
+        },
+    },
+});
