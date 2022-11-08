c7ca7606a2526e3e8d4d60679f6137459278c17c
Pascal Allau <pascal.allau@zohomail.eu>
2022-11-08 10:24:23 +0100

    refactor: rename keyword in fieldName

--- src/constructor/searchAdvancedField.js
+++ src/constructor/searchAdvancedField.js
@@ -1,49 +1,49 @@
-import { cloneTemplate } from '../utils/functions.js';
+import { cloneTemplate } from '../utils/functions';
 
 /**
  * It creates a dropdown menu with a search bar
- * @param {String} keyword - the keyword that will be displayed in the dropdown button
+ * @param {String} fieldName - the fieldName that will be displayed in the dropdown button
  * @param {Array} options - an array of strings
  */
-export default function Keyword(keyword, options) {
-    this.keyword = keyword;
+export default function SearchAdvancedField(fieldName, options) {
+    this.fieldName = fieldName;
     this.options = options;
-    this.list = document.getElementById('dropdown-keyword');
-    this.fragment = cloneTemplate('dropdown-keyword-template');
+    this.list = document.getElementById('dropdown-fieldName');
+    this.fragment = cloneTemplate('dropdown-fieldName-template');
     this.li = this.fragment.firstElementChild;
-    this.li.setAttribute('id', `${this.keyword.toNormalize()}`);
+    this.li.setAttribute('id', `${this.fieldName.toNormalize()}`);
     this.li
         .querySelector('.dropdown__btn')
-        .classList.add(this.keyword.toNormalize());
+        .classList.add(this.fieldName.toNormalize());
     this.btn = this.li.querySelector('.dropdown__btn');
-    this.btn.setAttribute('id', `btn-${this.keyword.toNormalize()}`);
-    this.btn.setAttribute('aria-label', this.keyword.toNormalize());
-    this.li.querySelector('#dropdown-btn-value').innerHTML = this.keyword;
+    this.btn.setAttribute('id', `btn-${this.fieldName.toNormalize()}`);
+    this.btn.setAttribute('aria-label', this.fieldName.toNormalize());
+    this.li.querySelector('#dropdown-btn-value').innerHTML = this.fieldName;
     this.content = this.li.querySelector('.dropdown__content');
-    this.content.setAttribute('id', `content-${this.keyword.toNormalize()}`);
+    this.content.setAttribute('id', `content-${this.fieldName.toNormalize()}`);
     this.content.setAttribute(
         'aria-labelledby',
-        `btn-${this.keyword.toNormalize()}`
+        `btn-${this.fieldName.toNormalize()}`
     );
     this.content.setAttribute('aria-expanded', 'false');
-    this.content.classList.add(this.keyword.toNormalize());
+    this.content.classList.add(this.fieldName.toNormalize());
     this.ul = this.content.querySelector('.dropdown__content__options');
     this.inputClass = this.content.querySelector('.dropdown__content__input');
-    this.inputClass.placeholder = `Rechercher un ${this.keyword
+    this.inputClass.placeholder = `Rechercher un ${this.fieldName
         .toLocaleLowerCase()
         .slice(0, -1)}`;
-    this.inputClass.setAttribute('id', `input-${this.keyword.toNormalize()}`);
+    this.inputClass.setAttribute('id', `input-${this.fieldName.toNormalize()}`);
     this.li
         .querySelector('.dropdown__content svg')
-        .setAttribute('id', `close-${this.keyword.toNormalize()}`);
-    this.ul.setAttribute('id', `options-${this.keyword.toNormalize()}`);
+        .setAttribute('id', `close-${this.fieldName.toNormalize()}`);
+    this.ul.setAttribute('id', `options-${this.fieldName.toNormalize()}`);
     this.list.appendChild(this.fragment);
-    this.btn = document.getElementById(`btn-${this.keyword.toNormalize()}`);
+    this.btn = document.getElementById(`btn-${this.fieldName.toNormalize()}`);
     this.contentKeyword = document.querySelector(
-        `#content-${this.keyword.toNormalize()}`
+        `#content-${this.fieldName.toNormalize()}`
     );
     this.close = this.contentKeyword.querySelector(
-        `#close-${this.keyword.toNormalize()}`
+        `#close-${this.fieldName.toNormalize()}`
     );
     this.btn.addEventListener('click', () => {
         document.querySelectorAll('.dropdown__content').forEach((elt) => {
@@ -52,18 +52,13 @@ export default function Keyword(keyword, options) {
                 elt.previousElementSibling.firstElementChild.style.display =
                     'flex';
             }
-            // elt.dataset.active === 'true' &&
-            //     (elt.dataset.active =
-            //         'false' &&
-            //         (elt.previousElementSibling.firstElementChild.style.display =
-            //             'flex'));
         });
         this.content.dataset.active === 'false'
             ? this.openDropdown()
             : this.closeDropdown();
     });
     this.inputId = this.content.querySelector(
-        `#input-${this.keyword.toNormalize()}`
+        `#input-${this.fieldName.toNormalize()}`
     );
     this.close.addEventListener('click', () => {
         this.content.dataset.active === 'false'
