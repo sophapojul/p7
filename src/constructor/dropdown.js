e3868152316ef855beb6fd9a86803d7d82c5498e
Pascal Allau <pascal.allau@zohomail.eu>
2022-11-08 10:16:34 +0100

    refactor: remove addEventListener in dropdown.js

--- src/constructor/dropdown.js
+++ src/constructor/dropdown.js
@@ -11,19 +11,10 @@ export default function Dropdown(name, options) {
     this.options.forEach((option) => {
         this.li = document.createElement('li');
         this.li.classList.add('dropdown__content__options__item');
+        this.li.dataset.selected = 'false';
         this.li.innerText = `${option}`;
         this.ul.appendChild(this.li);
     });
     this.li = this.ul.querySelectorAll('.dropdown__content__options__item');
     this.input = this.ul.parentElement.firstElementChild;
-    this.input.addEventListener('input', (ev) => {
-        this.li.forEach((item) => {
-            const { style, innerText } = item;
-            if (innerText.toNormalize().includes(ev.target.value)) {
-                style.display = 'block';
-            } else {
-                style.display = 'none';
-            }
-        });
-    });
 }
