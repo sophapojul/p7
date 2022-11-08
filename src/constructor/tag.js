d6039267c320c3c73b3418e6d9fb2c388ae08edf
Pascal Allau <pascal.allau@zohomail.eu>
2022-11-08 10:28:17 +0100

    feat: add type of tag

--- src/constructor/tag.js
+++ src/constructor/tag.js
@@ -1,14 +1,17 @@
+import { cloneTemplate } from '../utils/functions.js';
+
 /**
- * It takes a tag, creates a new list item, adds the tag to the list item, and adds a close button to the list item
+ * It takes a tag, creates a new list fragment, adds the tag to the list fragment, and adds a close button to the list fragment
  * @param {String} tag - the tag that will be displayed in the tag list
  */
-export default function Tag(tag) {
+export default function Tag(tag, type) {
     this.tag = tag;
-    this.template = document.querySelector('#tag-template');
-    this.list = document.querySelector('#tag-list');
-    this.item = this.template.content.cloneNode(true);
-    this.item.querySelector('.search__tag__item').innerText = this.tag;
-    this.list.appendChild(this.item);
+    this.type = type;
+    this.fragment = cloneTemplate('tag-template');
+    this.list = document.querySelector('#tag__list');
+    this.fragment.querySelector('.search__tag').classList.add(this.type);
+    this.fragment.querySelector('.search__tag__item').innerText = this.tag;
+    this.list.appendChild(this.fragment);
     this.close = this.list.querySelectorAll('svg');
     this.close.forEach((item) =>
         item.addEventListener('click', (ev) => {
